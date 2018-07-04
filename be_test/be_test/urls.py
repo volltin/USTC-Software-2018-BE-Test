37322ce4043ccdc229b34fa9da52e61a69462aaf
andytt <936664530@qq.com>
2018-07-04 11:21:12 +0800

    登陆模块

--- be_test/be_test/urls.py
+++ be_test/be_test/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from users.views import regis
+from users.views import regis,login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/register/', regis),
+    path('user/login/', login),
 ]
--- be_test/users/views.py
+++ be_test/users/views.py
@@ -1,8 +1,9 @@
 import json
+import datetime
 
 from django.shortcuts import render
 from django.http import HttpResponse
-from users.models import Users
+from users.models import Users 
 
 # Create your views here.
 
@@ -22,16 +23,51 @@ def regis(request):
 
     if len(username) > 30:
         ret["err_code"] = -1,
-        ret["err_msg"] = "Username is too long."
+        ret["err_msg"] = "用户名太长."
     elif len(password) > 50:
         ret["err_code"] = -2,
-        ret["err_msg"] = "Password is too long."
+        ret["err_msg"] = "密码太长."
     else:
         user,flag = Users.objects.get_or_create(name=username,password=password)
         if flag == False:
             ret["err_code"] = -3,
-            ret["err_msg"] = "Username has been existed."
+            ret["err_msg"] = "用户名已经存在."
     return HttpResponse(json.dumps(ret),content_type="application/json")
+
+def login(request):
+    '''
+    登陆
+    '''
+    username = request.POST['username']
+    password = request.POST['password']
+    login_time = int(request.POST.get("login_time","24")) # 登陆时长
+    ret = json_reg.copy()
+    try:
+        user = Users.objects.get(name=username,password=password)
+    except Exception as e:
+        user = None
+    if user:
+        request.session['login'] = True
+        request.session.set_expiry(3600 * login_time)   #设置登陆时长
+
+        if request.META.get('HTTP_X_FORWARDED_FOR',None):
+            ip =  request.META['HTTP_X_FORWARDED_FOR']
+        else:
+            ip = request.META['REMOTE_ADDR']
+        
+        user.update_ip = ip         # 获取ip
+        user.update_date = datetime.datetime.now()  # 获取时间
+    else:
+        ret["err_code"] = -1
+        ret["err_msg"] = "用户名或密码错误"
+    return HttpResponse(json.dumps(ret),content_type="application/json")
+
+
+
+
+
+
+
 
 
 
