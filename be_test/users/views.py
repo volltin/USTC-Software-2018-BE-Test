50a1b75c8b0ecda40fe90dbae7de514e0eea54ec
andytt <936664530@qq.com>
2018-07-04 10:25:23 +0800

    修改name为unique

--- be_test/users/views.py
+++ be_test/users/views.py
@@ -1,3 +1,19 @@
+import json
+
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+json_reg = {
+    "err_code":0,
+    "err_meg":"",
+}
+
+def regis(request):
+    '''
+    注册页面
+    '''
+    username = request.POST['username']
+    passwd = request.POST['password']
+    
